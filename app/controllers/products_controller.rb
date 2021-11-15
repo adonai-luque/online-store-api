876d5e9a7f0944846079a89c6449cbe3a7990250
adonai-luque <akluqueprieto@gmail.com>
2021-11-15 03:09:04 -0500

    products controller params permit modified

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,6 +46,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :url_image, :price, :discount, :category_id)
+      params.require(:product).permit(:name, :url_image, :price, :discount, :category)
     end
 end
